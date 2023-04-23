92a38b47b8eb64f40fc490d8060dafe5be9c1b68
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-23 13:09:01 +0300

    add ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,3 +1,23 @@
-<li class="gallery-item">
-  <img src="" alt="" />
-</li>
+import React from "react";
+import css from './ImageGalleryItem.module.css'
+
+const ImageGalleryItem = ({ image, onSelect }) => {
+  const { webformatURL, tags, id } = image;
+
+  const handleClick = () => {
+    onSelect(id);
+  };
+
+  return (
+    <li className={CSS.ImageGalleryItem}>
+      <img
+        src={webformatURL}
+        alt={tags}
+        className={css.ImageGalleryItem_image}
+        onClick={handleClick}
+      />
+    </li>
+  );
+};
+
+export default ImageGalleryItem;
