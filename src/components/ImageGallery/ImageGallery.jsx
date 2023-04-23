b18dead905ffc1d8049d9e07f0aa8e7129bfa622
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-23 13:08:33 +0300

    fix imagegalerry

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,19 @@
-<ul class="gallery">
-  {/* <!-- Набор <li> с изображениями --> */}
-</ul>
+import React from "react";
+import ImageGalleryItem from "../ImageGalleryItem";
+import css from './ImageGallery.module.css'
+
+const ImageGallery = ({ images, onSelect }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      {images.map((image) => (
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onSelect={onSelect}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ImageGallery;
